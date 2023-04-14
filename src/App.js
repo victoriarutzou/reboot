b5179ac47e54f2185ddfa37db0c34072f2728e2c
vic <victoria.rutzou@gmail.com>
2023-04-14 09:35:16 +0200

    version1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Forside from "./pages/Forside";
+import React from "react";
+import "./App.css";
+import "./index.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+     <Nav/>
+     <Routes>
+      <Route path="/" element={<Forside/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Forside.js
@@ -0,0 +1,34 @@
+export default function Forside(){
+
+
+
+    return (
+        <main>
+
+
+            <section className="hero">
+            </section>
+
+            <section className="bæredygtighed">
+            </section>
+
+            <section className="værktøjer">
+            </section>
+
+            <section className="gartnere">
+            </section>
+
+            <section className="stor_forskel">
+            </section>
+
+            <section className="kunder">
+            </section>
+
+            <section className="prisestimat">
+            </section>
+
+            <section className="kontakt">
+            </section>
+        </main>
+    )
+}
